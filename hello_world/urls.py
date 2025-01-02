17d59637b379c13658a9ffa1e41e3d3fcdc95863
deetonn <deetonrushton@gmail.com>
2025-01-02 10:04:52 +0000

    god only knows

--- hello_world/urls.py
+++ hello_world/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for my_project project.
+URL configuration for hello_world project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from hello_world import views as hello_views
 
 urlpatterns = [
+    path('', hello_views.index, name='index'),
     path('admin/', admin.site.urls),
 ]
