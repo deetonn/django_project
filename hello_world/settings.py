8be227abc1a6a0045ed6d4f1de2eb3cee9a9472d
deetonn <deetonrushton@gmail.com>
2025-01-02 10:16:10 +0000

    Add PROJECT_ROOT and STATIC_ROOT

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,6 +30,8 @@ ALLOWED_HOSTS = [
     '.herokuapp.com'
 ]
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # Application definition
 
