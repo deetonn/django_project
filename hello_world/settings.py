24444f5a4e931f363a9677645620682fa527c6e5
deetonn <deetonrushton@gmail.com>
2025-01-02 10:14:45 +0000

    Heroku business

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)2@)o+48$98hkrp^&d95a45c061rdtbdiptwuc07e)ir0w=9vx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.herokuapp.com'
+]
 
 
 # Application definition
